5f147c6ada18320a767d0bd9002046ce9281b728
Josh Pollock <josh@calderawp.com>
2019-02-25 15:35:33 -0500

    fix other tests

--- Caldera.php
+++ Caldera.php
@@ -1,17 +1,20 @@
 <?php
 
-/**
- * Contains the main instance of Caldera Core
- *
- * @return \calderawp\caldera\Core\CalderaCore
- */
-function caldera(){
-	static $caldera;
-	if( ! $caldera ){
-		$caldera = new \calderawp\caldera\Core\CalderaCore(
-			new \calderawp\CalderaContainers\Service\Container()
-		);
-	}
+if (!function_exists('caldera')) {
+	/**
+	 * Contains the main instance of Caldera Core
+	 *
+	 * @return \calderawp\caldera\Core\CalderaCore
+	 */
+	function caldera(){
+		static $caldera;
+		if( ! $caldera ){
+			$caldera = new \calderawp\caldera\Core\CalderaCore(
+				new \calderawp\CalderaContainers\Service\Container()
+			);
+		}
 
-	return $caldera;
+		return $caldera;
+	}
 }
+
